6a9f0679506265e4f407b0720e73e011917a1cd4
alyssacaples <alyssacaples@outlook.com>
2025-04-21 08:01:11 -0700

    quick galaxy output

--- wp_pdf_test.py
+++ wp_pdf_test.py
@@ -3,8 +3,8 @@ from weasyprint.text.fonts import FontConfiguration
 
 galaxy = HTML('galaxy.html') 
 font_config = FontConfiguration()
-css = CSS('galaxy.css')
-galaxy.write_pdf("galaxy_output.pdf", stylesheets = [css])
+css = CSS('galaxy.css', font_config=font_config)
+galaxy.write_pdf("galaxy_output.pdf", stylesheets = [css], font_config=font_config)
 
-# background: url("https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Hubble2005-01-barred-spiral-galaxy-NGC1300.jpg/2560px-Hubble2005-01-barred-spiral-galaxy-NGC1300.jpg");
+# 
 
