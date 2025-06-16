2328c7d0107eed344fd6d2cda1a71a0dad54646d
alyssacaples <alyssacaples@outlook.com>
2025-06-15 21:39:23 -0700

    Revert configuration files to 'trying again' commit state - preserve source code logic

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import commonjs from 'vite-plugin-commonjs';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(), 
-    commonjs(),
-    nodePolyfills({
-      include: ['buffer', 'process', 'util', 'stream'],
-      globals: {
-        Buffer: true,
-        global: true,
-        process: true,
-      },
-    })
-  ],
+  plugins: [react()],
   base: './',
   build: {
     outDir: 'dist',
@@ -43,7 +30,8 @@ export default defineConfig({
   },
   optimizeDeps: {
     // Force pre-bundling of problematic dependencies
-    include: ['emoji-regex'],
+    include: ['@react-pdf/renderer'],
+    exclude: [],
   },
   define: {
     // Fix for some CommonJS dependencies
